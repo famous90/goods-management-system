200471670546f118350949f9cdb0d715e156cd29
gyuyoung Hwang <famous90h@gmail.com>
2014-11-02 20:17:29 +0900

    no message

--- server.js
+++ server.js
@@ -26,8 +26,6 @@ var daumMapApiKey = 'e0d5fc7d17aa2f5abb974f8c0aebec4ebf66cf40';
 app.get('/brand', function(request, response){
         client.query('SELECT * FROM brand', function(error, data){
                      response.send(data);
-                     console.log('data: ' + data);
-                     console.log('error: ' + error);
                      });
         });
 
@@ -106,7 +104,6 @@ app.get('/data.redirect?', function(request, response) {
 app.get('/items', function(request, response){
         client.query('SELECT * FROM items', function(error, data){
                      response.send(data);
-                     console.log(data);
                      });
         });
 
